acbcc323b338726a0548c399d64e1d5813a94860
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-22 22:44:04 +0300

    Set TRACKS query to retrieve all tracks data

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+// TRACKS query to retrieve all tracks
+const TRACKS = gql`
+  query GetTracksData {
+    tracksForHome {
+      id
+      author {
+        id
+        name
+        photo
+      }
+      title
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
 const Tracks = () => {
   return <Layout grid> </Layout>;
 };
